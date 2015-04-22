8a24a9916ea8dcc7f2a748d6d37a88e0b669815a
sanjithdavid <sdavid@deloitte.com>
2015-04-22 17:10:08 -0400

    Test code for adding topics and subtopics

--- src/public/app/controllers/testCtrl.js
+++ src/public/app/controllers/testCtrl.js
@@ -1,3 +1,82 @@
-angular.module('app').controller('testCtrl', function($scope) {
+angular.module('app').controller('testCtrl', ['$scope',function($scope) {
+
+	$scope.topicValue={};
+	$scope.subTopicValue={};
+	$scope.document={
+  "_id": "11",
+  "eventName": "Japan Earthquake",
+  "eventId": "12345",
+  "eventInstanceId": "jp2",
+  "categories": {
+    "Web": {
+      "assignedTo": "John",
+      "displayValue": "Web"
+    },
+    "TV": {
+      "assignedTo": "John",
+      "displayValue": "TV"
+    },
+    "Print": {
+      "assignedTo": "John",
+      "displayValue": "Print"
+    }
+  }
+};
+
+
+$scope.addTopic=function(category)
+{
+	
+	var newTopic=$scope.topicValue[category];
+
+	if($scope.document.categories[category].topics==undefined)
+	{
+		$scope.document.categories[category].topics={};
+	}
+
+
+	if($scope.document.categories[category].topics[newTopic])
+	{
+		alert(newTopic+" already exists");
+	}
+	else
+	{
+		$scope.document.categories[category].topics[newTopic]={};
+		$scope.document.categories[category].topics[newTopic].displayValue=newTopic;
+	}
+	
+	console.log($scope.document);
+}
+
+$scope.addSubTopic=function(category,topic)
+{
+	console.log(category,topic);
+	console.log($scope.document);
+	
+	var newSubTopic=$scope.subTopicValue[category+'-'+topic];
+	console.log(newSubTopic,$scope.document.categories[category].topics[topic].subTopics);
+
+	if($scope.document.categories[category].topics[topic].subTopics==undefined)
+	{
+		$scope.document.categories[category].topics[topic].subTopics={};
+	}
+
+
+	if($scope.document.categories[category].topics[topic].subTopics[newSubTopic])
+	{
+		alert(newSubTopic+" already exists");
+	}
+	else
+	{
+		//$scope.document.categories[category].topics[topic].subTopics={};
+
+		$scope.document.categories[category].topics[topic].subTopics[newSubTopic]={}
+		$scope.document.categories[category].topics[topic].subTopics[newSubTopic].displayValue=newSubTopic;
+	}
+	
+
+}
+
+
   
-});
+}]);
