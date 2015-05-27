54cd65de30ac126c739ee0cbc85972d73bb75749
sanjithdavid <sdavid@deloitte.com>
2015-05-27 14:40:34 -0400

    Fixed sort by user name for the new logic

--- src/public/app/controllers/dashDraftsCtrl.js
+++ src/public/app/controllers/dashDraftsCtrl.js
@@ -104,21 +104,42 @@ $scope.sortInstances = function(sortType) {
 }
 
 function compareAsc(a,b) {
-  if ((a[$scope.sortType]).toString().toLowerCase() < (b[$scope.sortType]).toString().toLowerCase())
-    return -1;
-  if ((a[$scope.sortType]).toString().toLowerCase() > (b[$scope.sortType]).toString().toLowerCase())
-    return 1;
-  return 0;
+  if($scope.sortType=="coordinatorAssign")
+  {
+    if ((a.userCreated.displayName).toString().toLowerCase() < (b.userCreated.displayName).toString().toLowerCase())
+      return -1;
+    if ((a.userCreated.displayName).toString().toLowerCase() > (b.userCreated.displayName).toString().toLowerCase())
+      return 1;
+    return 0;
+  }
+  else
+  {
+    if ((a[$scope.sortType]).toString().toLowerCase() < (b[$scope.sortType]).toString().toLowerCase())
+      return -1;
+    if ((a[$scope.sortType]).toString().toLowerCase() > (b[$scope.sortType]).toString().toLowerCase())
+      return 1;
+    return 0;
+  }
 }
 
 function compareDesc(a,b) {
-  if ((a[$scope.sortType]).toString().toLowerCase() > (b[$scope.sortType]).toString().toLowerCase())
-    return -1;
-  if ((a[$scope.sortType]).toString().toLowerCase() < (b[$scope.sortType]).toString().toLowerCase())
-    return 1;
-  return 0;
+  if($scope.sortType=="coordinatorAssign")
+  {
+    if ((a.userCreated.displayName).toString().toLowerCase() > (b.userCreated.displayName).toString().toLowerCase())
+      return -1;
+    if ((a.userCreated.displayName).toString().toLowerCase() < (b.userCreated.displayName).toString().toLowerCase())
+      return 1;
+    return 0;
+  }
+  else
+  {
+    if ((a[$scope.sortType]).toString().toLowerCase() > (b[$scope.sortType]).toString().toLowerCase())
+      return -1;
+    if ((a[$scope.sortType]).toString().toLowerCase() < (b[$scope.sortType]).toString().toLowerCase())
+      return 1;
+    return 0;
+  }
 }
-
 
 $scope.pageCount = function () {
     return Math.ceil($scope.totalInstances / $scope.itemsPerPage);
