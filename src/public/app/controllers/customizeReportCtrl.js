51f27010ff1591b33d1d4f36200103fec4c24daf
Michael Ta <technical.ta@gmail.com>
2015-06-18 16:37:07 -0400

    [Pending] Sections are movable; loaded in the document; added check boxes; needs parent-child checkbox functionality

--- src/public/app/controllers/customizeReportCtrl.js
+++ src/public/app/controllers/customizeReportCtrl.js
@@ -15,15 +15,56 @@ angular.module('app').controller('customizeReportCtrl', function($scope) {
 			"editing": false
 		}]
 	};
-
+//For testing not useful
 	$scope.customizedDoc = [];
 	console.log($scope.eventdoc);
 	$scope.customizedDoc.push({sectionName: 'Document', sectionData:$scope.eventdoc});
 	$scope.customizedDoc.push({sectionName: 'Data', sectionData:$scope.eventData});
 	console.log($scope.customizedDoc);
 
-	for(var i = 0; i < $scope.customizedDoc.length; i++){
+	for(var i = 0; i < $scope.customizedDoc.length; i++) {
 		console.log($scope.customizedDoc[i].sectionName);
+		console.log($scope.customizedDoc[i].sectionData);
+		if($scope.customizedDoc[i].sectionName == 'Document'){
+			console.log($scope.customizedDoc[i].sectionData.categories[0]);
+
+		}
+		// for(var j = 0; j < $scope.customizedDoc[i].sectionData.length; i++) {
+		// 	console.log($scope.customizedDoc[i].sectionData[j]);
+		// }
 	}
+//
+	//Following two functions are for checking child boxes from parent
+	$scope.allNeedsClicked = function() {
+		var newValue = !$scope.allNeedsMet();
+
+		_.each($scope.todos, function(todo) {
+			todo.done = newValue;
+		});
+	};
+
+	// Returns true if and only if all todos are done.
+	$scope.allNeedsMet = function() {
+		var needsMet = _.reduce($scope.todos, function(memo, todo) {
+			return memo + (todo.done ? 1 : 0);
+		}, 0);
+
+		return (needsMet === $scope.todos.length);
+	};
+}
+
+	$scope.checkedValues = function(section) {
+		console.log(section.checked);
+	};
+
+
+//Movable UI section
+	$scope.editSection = function(section) {
+		section.editing = true;
+	};
+
+	$scope.cancelEditingTopic = function(topic) {
+		topic.editing = false;
+	};
 
 });
