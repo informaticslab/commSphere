8abd0e5d5fa97c52c308afb617651c556255399f
Michael Ta <technical.ta@gmail.com>
2015-06-22 16:14:07 -0400

    pending

--- src/public/app/controllers/customizeReportCtrl.js
+++ src/public/app/controllers/customizeReportCtrl.js
@@ -28,6 +28,68 @@ angular.module('app').controller('customizeReportCtrl', function($scope, $modal)
 		console.log(isChecked.checked);
 	};
 
+	$scope.getTableHeight = function(grid, id) {
+		var rowHeight = 30; // your row height
+		var headerHeight = 30; // your header height
+		//if (id.split('_')[1] ==='0') {
+		return {
+			height: ((grid.dailyData.length + 1) * rowHeight + headerHeight - 12) + "px"
+		};
+		//}
+		// else {
+		// return {
+		//    height: (grid.dailyData.length * rowHeight + headerHeight) + "px" };
+		// }
+	};
+
+	$scope.generateColumnDefs = function() {
+		var columnArry = [];
+		var columnLayout = [];
+		// pick a grid to iterate
+		var oneGrid = $scope.eventData.gridData[0];
+		if (oneGrid) { // at least one grid exist
+			for (var columnName in oneGrid.dailyData[0]) {
+				if (oneGrid.dailyData[0].hasOwnProperty(columnName)) {
+					if (columnName !== '$$hashKey' && columnName != 'label') {
+						columnArry.push(columnName);
+					}
+				}
+			}
+
+		} else {
+			$scope.addDataColumn('label');
+			// columnArry.push('label');
+			$scope.addDataColumn('' + $scope.eventdoc.dateCreated)
+			columnArry.push('' + $scope.eventdoc.dateCreated);
+		}
+		columnArry.sort();
+		columnArry.unshift('label');
+		for (i = 0; i < columnArry.length; i++) {
+			// build columns defition object
+			if (columnArry[i] === 'label') {
+				oneColumnDef = {
+					'field': columnArry[i],
+					enableSorting: false,
+					minWidth: $scope.minTopicWidth,
+					pinnedLeft: true
+				};
+			} else {
+				var formattedDate = $filter('date')(columnArry[i], 'mediumDate');
+				oneColumnDef = {
+					'field': columnArry[i],
+					'displayName': formattedDate,
+					enableSorting: false,
+					minWidth: $scope.minColWidth,
+					enablePinning: false
+				};
+			}
+			columnLayout.push(oneColumnDef);
+		}
+
+		return columnLayout;
+
+	};
+	
 	$scope.saveSection = function(section, e) {
 		// topic.save();
 		section.editing = false;
