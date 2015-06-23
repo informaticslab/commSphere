4304ad31d2519798eabd6bcdd6af3579efc80b81
Michael Ta <technical.ta@gmail.com>
2015-06-23 10:32:21 -0400

    pending

--- src/public/app/controllers/customizeReportCtrl.js
+++ src/public/app/controllers/customizeReportCtrl.js
@@ -1,8 +1,9 @@
 angular.module('app').controller('customizeReportCtrl', function($scope, $modal) {
 
 	$scope.customizedDoc = [];
-	$scope.customizedDoc.push({sectionName: 'Media Summaries', sectionType: 'Document', sectionData:$scope.eventdoc});
 	$scope.customizedDoc.push({sectionName: 'Metrics', sectionType: 'Metrics', sectionData:$scope.eventData});
+	$scope.customizedDoc.push({sectionName: 'Media Summaries', sectionType: 'Document', sectionData:$scope.eventdoc});
+	
 	
 	$scope.minColWidth = 110;
 	$scope.minTopicWidth = 200;
@@ -137,105 +138,6 @@ angular.module('app').controller('customizeReportCtrl', function($scope, $modal)
 
 	};
 
-	// $scope.getChartData = function() {
-	//   var chartCategories= [];
-	//   var chartCategoriesHeading= [];
-	//   var serieData = [];
-	//   var series = [];
-	//   var chartData = {};
-
-	//   for (i = 0; i < $scope.eventData.dailyData.length; i ++) {
-	//   var oneRow =  $scope.eventData.dailyData[i];
-	//   if (i == 0) { // pick first row and generate the chart categories
-	//       for (var oneCol in oneRow) {
-	//          if (oneCol !== '$$hashKey' && oneCol !=='subTopic') {
-	//           // reformat to display on chart
-	//             chartCategories.push(oneCol);
-	//             chartCategoriesHeading.push($filter('date')(oneCol,'d-MMM'));
-	//        }
-	//       }
-	//       chartCategories.sort();   // sort the remaining columns heading
-	//   }
-	//   serieName = oneRow['subTopic'];
-	//      for (var j=0 ; j < chartCategories.length; j++) { 
-	//            serieData.push(Number(oneRow[chartCategories[j]]));
-	//          }
-	//   var newSerie = {name: serieName, data: serieData}
-	//       series.push(newSerie);
-	//       serieData = [];
-	//        console.log(series);
-	//   }
-	//   chartData['xAxis'] = chartCategoriesHeading;
-	//   chartData['series'] = series;
-	//   console.log(chartData);
-	//   return  chartData;
-
-	// }
-
-
-	$scope.$on('uiGridEventEndCellEdit', function() {
-		//   $scope.chartData = $scope.getChartData();
-		//   console.log('chart data inside grid update ', $scope.chartData );
-		//   $scope.highChartConfig.series = $scope.chartData.series;
-
-	})
-
-	$scope.removeColumn = function() {
-		var lastColumnName = $scope.columns[$scope.columns.length - 1].field.toString();
-		if (lastColumnName !== 'label') {
-			$scope.columns.splice($scope.columns.length - 1, 1);
-			for (var i = 0; i < $scope.eventData.gridData.length; i++) {
-				for (var j = 0; j < $scope.eventData.gridData[i].dailyData.length; j++) {
-					if ($scope.eventData.gridData[i].dailyData[j].hasOwnProperty(lastColumnName)) {
-						delete $scope.eventData.gridData[i].dailyData[j][lastColumnName];
-					} else { // column not exists, add
-					}
-				}
-			}
-		}
-	}
-
-	$scope.addColumn = function() {
-		// assuming using eventInstanceId as column name
-		var newColumnName = '' + new Date().getTime();
-		var formattedDate = $filter('date')(newColumnName, 'mediumDate');
-		$scope.columns.push({
-			'field': newColumnName,
-			'displayName': formattedDate,
-			enableSorting: false,
-			minWidth: $scope.minColWidth,
-			enablePinning: false
-		});
-		$scope.addDataColumn(newColumnName);
-	}
-
-
-	$scope.splice = function() {
-		$scope.columns.splice(1, 0, {
-			field: 'company',
-			enableSorting: false
-		});
-	}
-
-	$scope.unsplice = function() {
-		$scope.columns.splice(1, 1);
-	}
-
-	$scope.addRow = function(grid, id) {
-		var n = grid.dailyData.length + 1;
-		grid.dailyData.push({
-
-		});
-		// var myGrid = angular.element( document.querySelector( '#'+id ) );
-		// myGrid.css('height',(n+2)*30);
-	};
-
-	$scope.removeLastRow = function(grid, id) {
-
-		var n = grid.dailyData.length;
-		grid.dailyData.pop();
-
-	}
 	$scope.getTableHeight = function(grid, id) {
 		var rowHeight = 30; // your row height
 		var headerHeight = 30; // your header height
